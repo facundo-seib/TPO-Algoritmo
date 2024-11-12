194c9619ce4da421588f64c85e0138eda1e73690
Facundo Seib <fseib@trans.com.ar>
2024-11-12 19:36:54 -0300

    quickfixes

--- fechas.py
+++ fechas.py
@@ -1,18 +1,23 @@
 def validar_fecha(fecha):
+    fecha_a_validar = fecha
 
-    dia=int(fecha[0:2])
-    while dia<1 or dia>31:
-        fecha=input("El día ingresado es invalido, ingrese la fecha nuevamente (DD/MM/AAAA): ")
-        dia=int(fecha[0:2])
+    dia=int(fecha_a_validar[0:2])
+    mes=int(fecha_a_validar[3:5])
+    año=int(fecha_a_validar[6:10])
 
-    mes=int(fecha[3:5])
-    while mes<1 or mes>12:
-        fecha=input("El mes ingresado es invalido, ingrese la fecha nuevamente (DD/MM/AAAA): ")
-        mes=int(fecha[3:5])
+    while (dia<1 or dia>31) or (mes<1 or mes>12) or (año<1900 or año>2025):
+        if dia<1 or dia>31:
+            print("El día ingresado es invalido")
 
-    año=int(fecha[6:10])
-    while año<1900 or año>2025:
-        fecha=input("El año ingresado es invalido, ingrese la fecha nuevamente (DD/MM/AAAA):")
-        año=int(fecha[6:10])
+        if mes<1 or mes>12:
+            print("El mes ingresado es invalido")
 
+        if año<1900 or año>2025:
+            print("El año ingresado es invalido")
+        
+        fecha_a_validar = input("Ingrese la fecha nuevamente (DD/MM/AAAA): ")   
+        dia=int(fecha_a_validar[0:2])
+        mes=int(fecha_a_validar[3:5])
+        año=int(fecha_a_validar[6:10])
+    return fecha_a_validar
 
